3f0a47d2526b583e561f7b5a8e5c07fcc607ce1f
AmazedWonder <giftedwonda@gmail.com>
2023-07-17 00:39:29 +0100

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    # Convert string to a list of characters
-    char_list = list(my_string)
-    # Remove all of 'c' and 'C' from the list
-    i = 0
-    while i < len(char_list):
-        if char_list[i] in 'cC':
-            char_list.pop(i)
-        else:
-            #  move to next char
-            i += 1
-    # Convert the list back to a string
-    new_string = ''.join(char_list)
-    return new_string
+    return "".join([char for char in my_string if char not in "cc"])
