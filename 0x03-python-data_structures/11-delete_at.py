009e89057cf53ac8405fcc361451b410e9bcc6b4
AmazedWonder <giftedwonda@gmail.com>
2023-07-17 04:04:51 +0100

    Delete at

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    # If the index is negative or out of range, return same list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    # Create a new list that contains all
+    # elements except elem at the given index
+    del(my_list[idx])
+    #  Return mofdified list
+    return my_list
