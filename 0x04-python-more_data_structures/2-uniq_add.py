b81cde3ee112dcb8657f871dd1811554c0ab579d
AmazedWonder <giftedwonda@gmail.com>
2023-07-23 09:33:07 +0100

    Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    # Create a new list of unique values using filter() and lambda
-    unique_val = list(filter(lambda x: my_list.count(x) == 1, my_list))
-
-    # Use map() to convert the unique values to integers
-    unique_int_val = list(map(int, unique_val))
+    # Create a new set to store unique values
+    unique_set = set(my_list)
 
     # Add up the unique values, use the built-in sum() function
-    unique_add = sum(unique_int_val)
+    unique_add = sum(unique_set)
 
     # Return the sum of unique values
     return unique_add
