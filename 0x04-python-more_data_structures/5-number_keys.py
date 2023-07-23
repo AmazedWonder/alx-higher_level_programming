b5d23830eef0a8a950d9bdf367a12726f82e5d80
AmazedWonder <giftedwonda@gmail.com>
2023-07-24 00:25:49 +0100

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    count_key = 0
+
+    #  loop through each key in the dictionary
+    for key in a_dictionary:
+
+        #   Incrementing count by 1 for each key
+        count_key += 1
+
+    #  The total number of keys in the dictionary
+    return count_key
