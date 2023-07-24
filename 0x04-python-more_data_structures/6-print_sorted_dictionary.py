84e43504dfec726d9ff4bf47b569476e6fdd90a5
AmazedWonder <giftedwonda@gmail.com>
2023-07-24 04:43:27 +0100

    Print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 #  defines a function name and takes my_dict as arg
-def print_sorted_dictionary(my_dict):
+def print_sorted_dictionary(a_dictionary):
 
     #  Using the sorted() function,create a sorted
     #  list of the keys in the dictionary my_dict
     #  Loop through each key in the sorted list
     #  and assign it to the variable k in each iteration
-    for key in sorted(my_dict.keys()):
+    for key in sorted(a_dictionary.keys()):
 
         #  Print each key-value pair in the dict in sorted order
         #  use the format() method to create a string that
         #  combines the key and value of each item in the dictionary
-        print("{}: {}".format(key, my_dict[key]))
+        print("{}: {}".format(key, a_dictionary[key]))
