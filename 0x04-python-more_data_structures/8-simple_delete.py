c7bb6f6d55ae788bc10fb02a44a66bd4f48e1d56
AmazedWonder <giftedwonda@gmail.com>
2023-07-24 01:43:27 +0100

    Simple delete by key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def simple_delete(my_dict, key=""):
+def simple_delete(a_dictionary, key=""):
     #  Pop() method removes the key-value pair of key from
     #  dictionary, and returns the value of the removed item
     #  Pass None as the default value,if key is not
     #  in the dictionary, pop() will return None
-    my_dict.pop(key, None)
+    a_dictionary.pop(key, None)
 
-    return my_dict
+    return a_dictionary
