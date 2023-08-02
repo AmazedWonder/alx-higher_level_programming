d46dffadb21fa8e362a3e653e30dcdc7f2db5c32
AmazedWonder <giftedwonda@gmail.com>
2023-08-02 04:29:19 +0100

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A class that represents a square."""
+    pass
+    """This is an empty class that doesn't define
+       any attributes or methods, but it can be used
+       as a blueprint to create instances of squares.
+    """
