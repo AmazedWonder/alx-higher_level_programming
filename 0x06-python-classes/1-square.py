54fd2ae1df296d7c36f9a6ae920187e264d1dc04
AmazedWonder <giftedwonda@gmail.com>
2023-08-02 08:45:17 +0100

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Define a class Square.
+
+The size of a square is crucial for a square, many things
+depend of it (area computation, etc.), so, as class builder,
+must control the type and value of this attribute. One way
+to have the control is to keep it privately.
+
+self is a conventionally used first parameter name in instance
+methods of a class. The self parameter holds a reference to the
+instance of the class that the method is being called on. It is
+used to access the instance's attributes and methods.
+
+Defines an __init__ method that takes an optional size argument
+and initializes the __size attribute with it, after validating that
+it's an integer and greater than or equal to zero, and raising an
+appropriate exception otherwise.
+
+"""
 
 
 class Square:
