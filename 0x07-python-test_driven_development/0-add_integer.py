64c1eb36843efd034173ba45aa6ea6bbe6fff3f6
AmazedWonder <giftedwonda@gmail.com>
2023-08-07 05:54:52 +0100

    create readme files and Integers addition .py/.txt files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+"""Defines add_integer function, a function that
+   takes two arguments, a and b, and performs addition on them.
+"""
+
+
+def add_integer(a, b=98):
+    """Return: int addtion of a and b.
+       Floats args are typecasted into ints before addition.
+       Raises:
+           TypeError: If either a or b is not an integer or a float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+        
+    a = int(a)
+    b = int(b)
+    
+    return a + b
