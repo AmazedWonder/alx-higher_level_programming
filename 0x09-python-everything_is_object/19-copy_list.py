92363da303e96e5cb442d1f5e256cc2bd33a327d
AmazedWonder <giftedwonda@gmail.com>
2023-08-15 14:34:27 +0100

    Copy a list object

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,20 +1,3 @@
 #!/usr/bin/python3
-"""Define a class copy_list that returns a copy of a list
-
-the function uses the copy() method of the list object a
-to create a copy of the list.
-
-"""
-
-
 def copy_list(the_list):
-    """
-    Returns a copy of the input list.
-
-    Args:
-        the_list (list): The input list to be copied.
-
-    Returns:
-        list: A new list containing the same elements as the input list.
-    """
     return the_list.copy()
