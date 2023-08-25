5cc9bb368caf1587fddedda22ea10615beae7da4
AmazedWonder <giftedwonda@gmail.com>
2023-08-25 23:38:01 +0100

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry.
+
+defines a class BaseGeometry that represents the base geometry.
+
+It simply uses the pass statement, which is a placeholder
+statement in Python that does nothing. This allows the class
+to be defined without any members.
+
+"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
