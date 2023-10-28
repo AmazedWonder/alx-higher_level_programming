fafbd29f10fba9412dd139e5abd5da7aa34a4729
AmazedWonder <giftedwonda@gmail.com>
2023-10-28 04:01:28 +0100

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,7 +5,11 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
