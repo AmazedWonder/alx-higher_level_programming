689fd51746d82d7b929fd722b25b835a39478eef
AmazedWonder <giftedwonda@gmail.com>
2023-11-05 20:04:33 +0100

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# Bash script that takes in a URL, sends a request to that
+# URL, and displays the size of the body of the response
 
 # Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Error: URL argument is required."
-    exit 1
-fi
+#if [ -z "$1" ]; then
+    #echo "Error: URL argument is required."
+    #exit 1
+#fi
 
 # Send request and retrieve the size of the response body
 size=$(curl -w "%{size_download}\n" "$1" -so /dev/null)
