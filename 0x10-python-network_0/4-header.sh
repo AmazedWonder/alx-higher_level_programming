259ebd3727d50151cd18dab70c6d9b9550aedbee
AmazedWonder <giftedwonda@gmail.com>
2023-11-21 07:00:43 +0100

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# cript that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Error: URL argument is required."
-    exit 1
-fi
-
-# Send GET request with custom header and display the response body
-response=$(curl -s -H "X-School-User-Id:98" "$1")
-echo "$response"
+# cript that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.Send GET request with custom header and display the response body
+curl -s -H "X-School-User-Id:98" "$1"
