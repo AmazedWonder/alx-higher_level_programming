2b1573621e734893e985f65020f2f7de4bdece5c
AmazedWonder <giftedwonda@gmail.com>
2023-11-21 06:58:17 +0100

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# script that takes in a URL and displays all HTTP methods the server will accept.
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Error: URL argument is required."
-    exit 1
-fi
-
-# Send HEAD request, retrieve "Allow" header, and display the allowed methods
-allowed_methods=$(curl -sI "$1" | grep Allow | cut -d ' ' -f2-)
-echo "$allowed_methods"
+# script that takes in a URL and displays all HTTP methods the server will accept.Send HEAD request, retrieve "Allow" header, and display the allowed methods
+curl -sI "$1" | grep Allow | cut -d ' ' -f2-
