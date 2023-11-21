29a1baaf7810c92393aaf9c050b99d6885411935
AmazedWonder <giftedwonda@gmail.com>
2023-11-21 06:41:08 +0100

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that
-
-# URL, and displays the size of the body of the response
-# Send request and retrieve the size of the response body
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -w "%{size_download}\n" "$1" -so /dev/null
