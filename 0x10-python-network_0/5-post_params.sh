7622e2c7c9a95ba6f02183c6977090dcb58c566d
AmazedWonder <giftedwonda@gmail.com>
2023-11-21 07:03:14 +0100

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Error: URL argument is required."
-    exit 1
-fi
-
-# Send POST request with form data and display the response body
-response=$(curl -s -X POST -d "subject=I will always be here for PLD&email=test@gmail.com" "$1")
-echo "$response"
+curl -s -X POST -d "subject=I will always be here for PLD&email=test@gmail.com" "$1"
