840d9f9437ea81d69e3433feaafd2e185d130df4
AmazedWonder <giftedwonda@gmail.com>
2023-12-08 13:46:27 +0100

    Click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Adds a click event listener to the <div> element with ID 'red_header'
+$('div#red_header').click(function() {
+    // Updates text color of the <header> element to red
+    $('header').css('color', '#FF0000');
+});
